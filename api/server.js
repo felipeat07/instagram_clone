4c86e84022aace0b941ec3607c1d0c009cd86f72
Felipe Tavares <felipeat07@gmail.com>
2022-05-01 21:16:52 +0100

    incluindo o metodo GET e Post na api

--- api/server.js
+++ api/server.js
@@ -12,4 +12,54 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
 console.log('Servidor HTTP está escutando na porta ' + port);
+
+app.get('/', function(req,res){
+    res.send('Olá');
+});
+
+//POST faz a criação do documento
+app.post('/api', function(req, res){
+    var dados = req.body;
+    
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json({'Status': 'Error'});
+                }else{
+                    res.json({'Status': 'Inclusão realizada com sucesso'});
+                }
+
+                mongoclient.close();
+            });
+        });
+    });
+
+
+});
+
+//GET faz a leitura do documento
+app.get('/api', function(req, res){  
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find().toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(results);
+                }
+
+                mongoclient.close();
+            });
+        });
+    });
+
+
+});
